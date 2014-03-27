36be475a3aa6b7dc4ba08311649fe9c2adb72cb5
EPHorizon <ephorizons@gmail.com>
2014-03-26 21:23:38 -0600

    Bug fixes, added comments, and changed formatting

--- XbeeControl/BotSide/BotSide.ino
+++ XbeeControl/BotSide/BotSide.ino
@@ -1,53 +1,70 @@
+/*
+
+This sketch acts as the RedBot side for an Xbee communications
+link with another arduino or a computer. The RedBot should have
+its serial switch set to "Xbee HW Serial" in order to work.
+
+Coded by Robert Davis
+
+*/
+
 #include <RedBot.h>
 
 #define FORWARD 0x1
 #define BACKWARD 0x2
 #define RIGHT 0x3
-#define LEFT 0x4
+#define LEFT 0x4 
 #define BANK_RIGHT 0x5
-#define BANK_LEFT 0x5
+#define BANK_LEFT 0x6
 
 RedBotMotor motor;
-RedBotSoftwareSerial link;
 
 
-void setup() {
-  link.begin(9600);
-
+void setup() 
+{
+  Serial.begin(57600);  //Set to whatever baudrate your Xbee is configured to
 }
 
-void loop() {
-  
-  int distanceValue = (analogRead(A2));
-  
-  if (Serial.available() > 0){
+void loop() 
+{  
+  if (Serial.available() > 0)
+  {
     executeCommand();
+  }
+  else
+  {
+    motor.stop();
   }
 }
   
   
-void executeCommand(){
+void executeCommand()
+{
   int command = Serial.read();
   
-  if (command == BANK_RIGHT){
+  if (command == BANK_RIGHT)
+  {
     motor.rightDrive(128);
     motor.leftDrive(255);
   }
-  else if (command == BANK_LEFT){
+  else if (command == BANK_LEFT)
+  {
     motor.rightDrive(255);
     motor.leftDrive(128);
   }
-  else if (command == FORWARD){
+  else if (command == FORWARD)
+  {
     motor.rightDrive(255);
     motor.leftDrive(255);
   }
-  else if (command == RIGHT){
+  else if (command == RIGHT)
+  {
     motor.rightDrive(-255);
     motor.leftDrive(255);
   }
-  else if (command == LEFT){
+  else if (command == LEFT)
+  {
     motor.rightDrive(255);
     motor.leftDrive(-255);
   }
-  
 }
--- XbeeControl/ControlPanel/ControlPanel.ino
+++ XbeeControl/ControlPanel/ControlPanel.ino
@@ -1,44 +1,58 @@
+/*
+
+This sketch acts as the Arduino side for an Xbee communications
+link to a RedBot.
+
+Coded by Robert Davis
+
+*/
+
 #define FORWARD 0x1
 #define BACKWARD 0x2
 #define RIGHT 0x3
 #define LEFT 0x4
 #define BANK_RIGHT 0x5
-#define BANK_LEFT 0x5
+#define BANK_LEFT 0x6
 
 const int forwardButton = 3;
 const int rightButton = 2;
 const int leftButton = 4;
 
 
-void setup() {
-  Serial.begin(9600);
+void setup() 
+{
+  Serial.begin(57600); //Set to whatever baudrate your Xbee is configured to
   pinMode(forwardButton, INPUT);
   pinMode(rightButton, INPUT);
   pinMode(leftButton, INPUT);
 }
 
-void loop() {
+void loop() 
+{
   int forwardState, rightState, leftState;
   
   forwardState = digitalRead(forwardButton);
   rightState = digitalRead(rightButton);
   leftState = digitalRead(leftButton);
   
-  if ((forwardState == LOW) && (rightState == LOW)){
+  if ((forwardState == LOW) && (rightState == LOW))
+  {
     Serial.write(BANK_RIGHT);
   }
-  else if ((forwardState == LOW) && (leftState == LOW)){
+  else if ((forwardState == LOW) && (leftState == LOW))
+  {
     Serial.write(BANK_LEFT);
   }
-  else if (forwardState == LOW){
+  else if (forwardState == LOW)
+  {
     Serial.write(FORWARD);
   }
-  else if (rightState == LOW){
+  else if (rightState == LOW)
+  {
     Serial.write(RIGHT);
   }
-  else if (leftState == LOW){
+  else if (leftState == LOW)
+  {
     Serial.write(LEFT);
   }
-    
-  
 }
